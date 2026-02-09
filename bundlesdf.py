a7f285d3646ce03bd11a3c6eb214dfaa3bac64ee
Your Name <you@example.com>
2026-02-09 14:21:37 +1100

    fix

--- bundlesdf.py
+++ bundlesdf.py
@@ -5,7 +5,7 @@
 # and any modifications thereto.  Any use, reproduction, disclosure or
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
-
+# fmt: off
 
 from Utils import *
 from nerf_runner import *
